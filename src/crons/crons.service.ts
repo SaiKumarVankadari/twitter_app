da53b524dfff3bb9efd9a3e18c1d43898ba4b645
Vankadari Sai Kumar <vankadarisaikumar@Vankadaris-MacBook-Pro.local>
2023-09-28 16:44:26 +0530

    custom winston logger

--- src/crons/crons.service.ts
+++ src/crons/crons.service.ts
@@ -1,12 +1,13 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
+import { LoggerService } from 'src/logger.service';
 
 @Injectable()
 export class CronsService {
-    constructor(){}
-    private readonly logger = new Logger();
-    @Cron('*/10 */30 10-17 * * *')
+    constructor(private readonly xapp: LoggerService){}
+    // private readonly logger = new Logger(CronsService.name);
+    @Cron('*/10 * 10-17 * * *')
   handleCron() {
-    this.logger.debug('hey there! I am your CRON JOB at 10th second for every 10 minutes');
+    this.xapp.debug('hey there! I am your CRON JOB at 10th second for every 10 minutes');
   }
 }
